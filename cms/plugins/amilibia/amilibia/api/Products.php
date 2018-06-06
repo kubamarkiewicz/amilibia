8275ccecc45510cc430c802d116d12ff09207a32
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-06-06 16:31:16 +0200

    product icons

--- cms/plugins/amilibia/amilibia/api/Products.php
+++ cms/plugins/amilibia/amilibia/api/Products.php
@@ -17,10 +17,13 @@ class Products extends Controller
         $result = $query->get(); 
 
         if ($result) foreach ($result as $i => $item) {
-        	if ($item->images) foreach ($item->images as $j => $image) {
-        		$result[$i]['images'][$j]['path_resized'] = $image->getThumb(null, 400);
-        		$result[$i]['images'][$j]['path_resized_2x'] = $image->getThumb(null, 800);
-        	}
+            if ($item->icon) {
+                $result[$i]['icon']['path_resized'] = $item->icon->getThumb(null, 160);
+            }
+            if ($item->images) foreach ($item->images as $j => $image) {
+                $result[$i]['images'][$j]['path_resized'] = $image->getThumb(null, 400);
+                $result[$i]['images'][$j]['path_resized_2x'] = $image->getThumb(null, 800);
+            }
         }
 
         return response()->json($result, 200, array(), JSON_PRETTY_PRINT);
